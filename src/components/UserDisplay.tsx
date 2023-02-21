d4a8dccc82b57e59bdd74036026b3510d40f171a
T.Yajima <tatsurou.yajima@gmail.com>
2023-02-21 20:38:55 +0900

    :sparkles: メッセージ表示機能を実装

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,4 +1,5 @@
 import { User } from '../types'
+import MessageDisplay from './MessageDisplay'
 
 type Props = {
     user: User
@@ -10,6 +11,9 @@ function UserDisplay({ user }: Props) {
             <p className='text-xl text-gray-200 font-bold'>
                 {user.name}
             </p>
+        </div>
+        <div>
+            {user.messages.map((message, i) => <MessageDisplay key={i} index={i} message={message} />)}
         </div>
     </div>
 }
